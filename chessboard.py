45f023dd3fdd5de49986b09d8de2f773f0b639fa
TheOriginalBabu <51098394+TheOriginalBabu@users.noreply.github.com>
2021-11-09 21:46:27 -0500

    slepy

--- chessboard.py
+++ chessboard.py
@@ -4,19 +4,21 @@
 # Energy: None
 # - sorry for falling asleep in class
 
-def sizeinput():
+# <- INPUT ->
+
+def sizeinput(): # Input for size of chessboard
 	global xsize
 	global ysize
 	try:
 		xsize = int(input("Enter desired length of X Axis on rectangle shape: "))
-		if xsize < 1:
-			1/0
+		if xsize < 1: # Break condition
+			1/0 
 
 		ysize = int(input("Enter desired length of Y Axis on rectangle shape: "))
-		if ysize < 1:
+		if ysize < 1: # Break condition
 			1/0
 
-	except:
+	except: # Error catching
 		print("Invalid Input")
 		print()
 		sizeinput()
@@ -26,71 +28,95 @@ def squareinput():
 	global ycoord
 	try:
 		xcoord = int(input("Enter X Co-ordinate: "))
-		if xcoord < 1 or xcoord > xsize:
+		if xcoord < 1 or xcoord > xsize: # Break condition
 			1/0
 
 		ycoord = int(input("Enter Y Co-ordinate: "))
-		if ycoord < 1 or ycoord > ysize:
+		if ycoord < 1 or ycoord > ysize: # Break condition
 			1/0
 
-	except:
+	except: # Error catching
 		print("Invalid Input")
 		print()
 		squareinput()
-
-sizeinput()
-squareinput()
-
-
-if xcoord % 2 - ycoord % 2 == 0:
-	color = "black"
-else:
-	color = "white"
-
-print(f"Selected Co-ordinates ({xcoord}, {ycoord}) correspond to a {color} square")
-print()
-
-# Chess expansion
 
 def moveinput():
 	global xmove
 	global ymove
 	try:
 		xmove = int(input("Enter X Co-ordinate of position to move to: "))
-		if xmove < 1 or xmove > xsize:
+		if xmove < 1 or xmove > xsize: # Break condition
 			1/0
 
 		ymove = int(input("Enter Y Co-ordinate of position to move to: "))
-		if ymove < 1 or ymove > ysize:
+		if ymove < 1 or ymove > ysize: # Break condition
 			1/0
 
-	except:
+	except: # Error catching
 		print("Invalid Input")
 		print()
 		moveinput()
 
+sizeinput()
+squareinput()
 moveinput()
 
-# Checks
+# <- PROCESSING ->
+
+if xcoord % 2 - ycoord % 2 == 0: # Square color check
+	color = "black"
+else:
+	color = "white"
+
+if xmove % 2 - ymove % 2 == 0: # Square color check (second selection)
+	colornew = "black"
+else:
+	colornew = "white"
+
+chesspieces = [] # List initialization to hold all applicable pieces
+
+# Check if both co-ordinates the same
+if ycoord == ymove and xcoord == xmove:
+	print("Both sets of co-ordinates correspond to the same square. No applicable chess moves.")
+	exit()
 
 # Pawn
 if ycoord + 1 == ymove and xcoord == xmove:
-	print("pawncandoit")
+	chesspieces.append("Pawn")
 elif ycoord + 2 == ymove and xcoord == xmove:
-	print("pawnmightdoitifitsthefirstmoveofitself")
+	chesspieces.append("Pawn")
+	pawnassumption = True
+
 
 # Rook
 if abs((xcoord - xmove)) == 1 and ycoord == ymove or abs((ycoord - ymove)) == 1 and xcoord == xmove or xcoord == xmove or ycoord == ymove:
-	print("rookcandoit")
+	chesspieces.append("Rook")
 
-# Horse
+# Knight (Horse)
 if abs(xcoord - xmove) == 1 and abs(ycoord - ymove) == 2 or abs(xcoord - xmove) == 2 and abs(ycoord - ymove) == 1:
-	print("horsecandoit")
+	chesspieces.append("Knight")
 
 # Bishop
 if abs((xcoord - xmove)) == abs((ycoord - ymove)):
-	print("bishopcandoit")
+	chesspieces.append("Bishop")
 
-# king
+# King
 if abs((xcoord - xmove)) == 1 or abs((ycoord - ymove)) == 1:
-	print("kingcandoit")
+	chesspieces.append("King")
+
+# Queen (combination of either Rook, Bishop, and King)
+if "Rook" in chesspieces or "Bishop" in chesspieces or "King" in chesspieces:
+	chesspieces.append("Queen")
+
+# <- OUTPUT ->
+
+print(f"Selected Co-ordinates ({xcoord}, {ycoord}) correspond to a {color} square")
+print()
+
+print(f"Selected Co-ordinates of second position ({xmove}, {ymove}) correspond to a {colornew} square")
+print()
+
+print(f"Valid chess pieces to move to this location: {chesspieces}")
+
+if pawnassumption == True:	# Pawn can move up two if it's its first move,
+	print("Note: Assuming it's Pawn's first move")
